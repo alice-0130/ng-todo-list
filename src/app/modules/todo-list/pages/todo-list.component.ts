c344c2e2e45f93ee7a1f35c522392db2bd58590c
alice-0130 <nhy990130@sl-platform.com>
2023-12-11 14:57:30 +0900

    feat: format today date

--- src/app/modules/todo-list/pages/todo-list.component.ts
+++ src/app/modules/todo-list/pages/todo-list.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  today: Date;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.today = new Date();
   }
 
+  ngOnInit(): void {}
 }
