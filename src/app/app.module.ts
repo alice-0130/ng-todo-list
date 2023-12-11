bd168dc8738ac215ebf0025d1f9ced3c1edbbb11
alice-0130 <nhy990130@sl-platform.com>
2023-12-11 13:02:38 +0900

    feat: set todo-list module router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './modules/todo-list/pages/todo-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodoListComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'todo-list', component: TodoListComponent },
+      { path: '', redirectTo: '/todo-list', pathMatch: 'full' },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
