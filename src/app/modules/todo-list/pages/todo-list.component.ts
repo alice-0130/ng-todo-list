19c9bb811ac533a5b726223a66e2a38c0f7d9d05
alice-0130 <nhy990130@sl-platform.com>
2023-12-12 10:14:04 +0900

    feat: change todo value as observable

--- src/app/modules/todo-list/pages/todo-list.component.ts
+++ src/app/modules/todo-list/pages/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoListItem } from '../types/todo-list-item.type';
 import { TodoService } from 'src/app/services/todo.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'todo-list-page',
@@ -10,11 +11,13 @@ import { TodoService } from 'src/app/services/todo.service';
 })
 export class TodoListPage implements OnInit {
   today: Date;
-  items: TodoListItem[] = [];
+  // items: TodoListItem[] = [];
+  items$: Observable<TodoListItem[]>;
 
   constructor(private todoService: TodoService) {
     this.today = new Date();
-    this.todoService.todoItems$.subscribe((items) => (this.items = items));
+    this.items$ = this.todoService.todoItems$;
+    // this.todoService.todoItems$.subscribe((items) => (this.items = items));
   }
 
   ngOnInit(): void {}
